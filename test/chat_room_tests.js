5858d55b1f08f23c8bdf52b33ab1e1fa435cd151
Dan Oved <stangogh@gmail.com>
2012-04-27 14:34:08 -0400

    refactored adding messages to setup method

--- test/chat_room_tests.js
+++ test/chat_room_tests.js
@@ -49,17 +49,41 @@ suite('chatRoom.addMessage', function(){
 });
 
 suite('chatRoom.getMessagesSince', function(){
+    setup(function(done){
+        this.room = Object.create(chatRoom),
+        this.user = 'cjno',
+        this.msg1 = 'msg',
+        this.msg2 = 'msg2',
+        this.msg3 = 'msg3';
+        var self = this;
+
+        this.room.addMessage(this.user, this.msg1, function(e, data1){
+            self.data1 = data1;
+            this.room.addMessage(this.user, this.msg2, function(e, data2){
+                self.data2 = data2;
+                this.room.addMessage(this.user, this.msg3, function(e, data3){
+                    self.data3 = data3;
+                    done();
+                })
+            }.bind(this));
+        }.bind(this));
+    })
     test('should get message since given id', function(done){
        var room = Object.create(chatRoom),
            user = 'cjno';
-        room.addMessage(user, 'msg', function(e, first){
-           room.addMessage(user, 'msg2', function(e, second){
-               room.getMessagesSince(first.id, function(e, msgs){
-                   assert.isArray(msgs);
-                   assert.deepEqual(msgs, [second]);
-                   done();
-               })
-            });
-        });
+        this.room.getMessagesSince(this.data1.id, function(e, msgs){
+           assert.isArray(msgs);
+           assert.deepEqual(msgs, [this.data2, this.data3]);
+           done();
+        }.bind(this));
+    });
+    test('should yield an empty array if messages array does not exist', function(done){
+        done();
+    });
+    test('should yield an empty array if no relevant messages exist', function(done){
+        done();
+    });
+    test('should not throw exceptions if no callback provided', function(done){
+        done();
     });
 })
