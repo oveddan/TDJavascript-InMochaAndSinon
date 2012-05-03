05f66101b949050f6f08c00ffc9880c48a635ec8
Dan <dan@ubuntu.(none)>
2012-05-03 13:10:26 -0400

    Added packages.json, removed run_tests.js since not used

--- run_tests.js
+++
@@ -1,6 +0,0 @@
-//require.paths.push(--dirname);
-//require.paths.push(--dirname + "/deps");
-//require.paths.push(--dirname + "/lib");
-var reporter = require('nodeunit').reporters.default;
-process.chdir(__dirname);
-reporter.run(['test/chapp']);
